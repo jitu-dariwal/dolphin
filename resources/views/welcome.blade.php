57a379f9972fcbdc359cb96d5d8723bae40bbf43
jitu-dariwal <56591158+jitu-dariwal@users.noreply.github.com>
2019-11-22 18:23:34 +0530

    Change FAQ to Help

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
-					<a href="{{ url('/faq') }}">FAQ</a>
+					<a href="{{ url('/faq') }}">Help</a>
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
